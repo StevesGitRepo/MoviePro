bed342fafa619a467445e9e6d19f496ea27814b8
StevesGitRepo <steve.k.szymanski@gmail.com>
2023-03-17 08:26:13 -0700

    Movie types added

---
+++ Models/Database/Movie.cs
@@ -0,0 +1,45 @@
+﻿using MoviePro.Enums;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MoviePro.Models.Database
+{
+    public class Movie
+    {
+        //this is the primary key:
+        public int Id { get; set; }
+        //the Id as it exists in IMDB's system:
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string TagLine { get; set; }
+        public string Overview { get; set; }
+        public int Runtime { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        public MovieRating Rating { get; set; }
+        public float VoteAverage { get; set; }
+
+        [Display(Name = "Poster")]
+        public byte[] Poster { get; set; }
+        public string PosterType { get; set; }
+
+        [Display(Name = "Backdrop")]
+        public byte[] Backdrop { get; set; }
+        public string BackdropType { get; set; }
+
+        public string TrailerUrl { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Poster Image")]
+        public IFormFile PosterFile { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Backdrop Image")]
+        public IFormFile BackdropFile { get; set; }
+    }
+}
