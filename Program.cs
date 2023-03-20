656490b1919bf33d4b61e03ebda1e9c0557ba38d
StevesGitRepo <steve.k.szymanski@gmail.com>
2023-03-19 22:45:09 -0700

    merge

--- Program.cs
+++ Program.cs
@@ -1,9 +1,74 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
 using MoviePro.Data;
+using MoviePro.Models;
 using MoviePro.Models.Settings;
+using MoviePro.Services;
+using MoviePro.Services.Interfaces;
+using MoviePro.Helpers;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+/*var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+*/
+var connectionString = ConnectionService.GetConnectionString(builder.Configuration);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+// Custom Services
+builder.Services.AddScoped<IImageService, ImageService>();
+builder.Services.AddScoped<IAddressBookService, AddressBookService>();
+builder.Services.AddScoped<IEmailSender, EmailService>();
+
+builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+
+var app = builder.Build();
+var scope = app.Services.CreateScope();
+//get the database updated with the latest migrations
+await DataHelper.ManageDataAsync(scope.ServiceProvider);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/Home/HandleError/{0}");
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
+
+/*var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -46,3 +111,4 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
+*/
